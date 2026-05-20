e495ad80dd2298e68534ebfb137c5dd251632f82
Wanessa Vieira <wanessahonorato2006@gmail.com>
2026-05-20 10:08:33 -0300

    Update Contagem.java

--- entregavel/Contagem.java
+++ entregavel/Contagem.java
@@ -1,4 +1,4 @@
-public class ContagemSimples {
+public class Contagem {
     public static void main(String[] args) {
         // O laço começa em 1 e vai até 10, somando +1 a cada volta
         for (int i = 1; i <= 10; i++) {
